cd97ce16c71f191fa462ffb43b4285114066634d
Jason Breedlove <j@jasonbreedlove.com>
2022-04-24 14:54:27 -0700

    small style improvements, added entry object and expense array

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 'use strict';
 
+let expense = []
+function addExpense(event) {
+    event.preventDefault();
+
+    const entry = {
+        id: Date.now(),
+        type: document.getElementById('typeList').value,
+        spent: document.getElementById('spentOn').value,
+        date: document.getElementById('spentDate').value,
+        amount: document.getElementById('spentAmount').value,
+    }
+    expense.push(entry)
+    console.log(expense)
+}
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('addExpense').addEventListener('click', addExpense)
+})
+for(let i = 0; i < expense.length; i++){
+    console.log(expense[i])
+}
