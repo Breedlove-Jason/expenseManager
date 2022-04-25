672d598ce6d93145ecf7f30afb367cbec81dfcb3
Jason Breedlove <j@jasonbreedlove.com>
2022-04-25 16:04:49 -0700

    delete button
    delete button added and small styling and html changes

--- script.js
+++ script.js
@@ -13,23 +13,28 @@ function addExpense(event) {
         amount: document.getElementById('spentAmount').value,
     }
 
-    // console.log(expense);
     Object.values(entry).forEach(element => {
         expense.push(element);
-        // console.log(element);
-
-        // console.log(expense);
 
         document.forms[0].reset();
     });
     for(let i = 0; i < 1; i++) {
         const table = document.getElementById('expenseList')
-        const tr = document.createElement('tr');
+        const tr = document.createElement('TR');
+        const deleteButton = document.createElement('BUTTON')
+        deleteButton.className = 'deleteButton'
+        deleteButton.innerText = '!'
+        tr.appendChild(deleteButton);
+        deleteButton.addEventListener('click', function(event){
+            event.preventDefault();
+            event.target.parentElement.remove()
+            expense.splice(i, 1)
+        })
         for (let j = 0; j < expense.length; j++) {
             // const table = document.getElementById('expenseList')
             // const tr = document.createElement('tr');
             const tableRow = table.appendChild(tr);
-            const td = document.createElement('td');
+            const td = document.createElement('TD');
             td.innerHTML = expense[j];
             const tableData = tableRow.appendChild(td);
         }
@@ -40,32 +45,3 @@ function addExpense(event) {
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('addExpense').addEventListener('click', addExpense)
 });
-// console.log(expense);
-// for(let i = 0; i < expense.length; i++){
-//     console.log(expense[i])
-// }
-
-
-// add a new table row and 4 td fields for each entry
-// const tableData = tableRow.appendChild(td);
-// console.log(tableData);
-// for (let i = 0; i < expense.length; i++) {
-//     const table = document.getElementById('expenseList')
-//     const tr = document.createElement('tr');
-//     const tableRow = table.appendChild(tr);
-//     const td = document.createElement('td');
-//     td.innerHTML = expense[i];
-//     console.log(td);
-//     const tableData = tableRow.appendChild(td);
-    // tr.appendChild(td);
-    // const td2 = document.createElement('td');
-    // td2.innerHTML = expense[i].spent;
-    // tr.appendChild(td2);
-    // const td3 = document.createElement('td');
-    // td3.innerHTML = expense[i].date;
-    // tr.appendChild(td3);
-    // const td4 = document.createElement('td');
-    // td4.innerHTML = expense[i].amount;
-    // tr.appendChild(td4);
-// }
-// delete an entry
